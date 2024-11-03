6519a2b1f14e0c169a8f7263247bdd6dd313b079
nitarahmawati19 <nita.223040030.mail.unpas.ac.id>
2024-11-03 12:29:29 +0700

    Video 3 - Struktur Folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About Me</h1>
+    <h3>Owner: {{ $nama }}</h3>
+    <h4>Sainz's Victory in Australia 2024</h4>
+    <img src="img/carlos.jpg" alt="Carlos Sainz" width="200">
+
+
+</body>
+
+
+</html>
